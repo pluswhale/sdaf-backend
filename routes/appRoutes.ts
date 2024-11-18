81a2de6b971356fef41443411e65d617e523b139
SiarheiLabodzin <siarhei.labodzin@innowise.com>
2024-11-18 17:26:33 +0300

    changed login service with header jwt bearer

--- routes/appRoutes.ts
+++ routes/appRoutes.ts
@@ -19,6 +19,8 @@ import {
   stopAutoTransaction,
 } from '../controllers/autoTransactor';
 import { authenticate } from '../middlewares';
+import { getUserBalanceWithoutWalletId } from '../controllers/getUserBalanceCeffu';
+import { getWalletList } from '../controllers/getWalletList';
 
 const router = express.Router();
 
@@ -40,6 +42,11 @@ router.get('/auto-send/transactions/drop-all', authenticate, dropAllAutoTransact
 router.get('/quoting-engine/margins', getAllMarginsController);
 router.put('/quoting-engine/margins/:id', validateEditMargin, editMarginController);
 router.get('/quoting-engine/orders', getOrders);
+
+// CEFFU Prime Wallets Balances
+
+router.get('/balance', getUserBalanceWithoutWalletId);
+router.get('/wallet-ceffu', getWalletList);
 
 export default router;
 
