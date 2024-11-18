8189d1b8d416fc2c55565cb369327924ef9990a5
Egor Dultsev <listenmy@yandex.ru>
2024-11-18 13:08:26 +0300

    edit sotrted

--- services/getAllMargins.ts
+++ services/getAllMargins.ts
@@ -3,6 +3,25 @@ import { Margin } from '../db/entities';
 
 export const getAllMargins = async (dataSource: DataSource): Promise<Margin[]> => {
   const marginRepository = dataSource.getRepository(Margin);
-  return marginRepository.find();
+  const margins = await marginRepository.find();
+
+  const sortedMargins = margins.sort((a, b) => {
+    // Remove currency symbols and spaces, then split the range
+    const [aStart] = a.range
+      .replace(/[^0-9\-]/g, '')
+      .split('-')
+      .map(Number);
+    const [bStart] = b.range
+      .replace(/[^0-9\-]/g, '')
+      .split('-')
+      .map(Number);
+
+    // Compare the starting values numerically
+    return aStart - bStart;
+  });
+
+  console.log('sortedMargins', sortedMargins);
+
+  return sortedMargins;
 };
 
