3093360c417cc4d5a1cb7c0108884f77dd069da6
SiarheiLabodzin <siarhei.labodzin@innowise.com>
2024-11-26 11:14:47 +0300

    changed getUserBalanceCeffu

--- controllers/getUserBalanceCeffu.ts
+++ controllers/getUserBalanceCeffu.ts
@@ -54,23 +54,15 @@ export const getUserBalance = async (req: Request, res: Response): Promise<void>
 
       if (response.data.code === '000000') {
         const assetsData = response.data.data?.data || [];
-        res.status(200).json({ balances: assetsData });
-        // const assets = assetsData.map((asset: any) => asset.coinSymbol);
-        // const usdPrices = await fetchUsdPrices(assets);
 
-        // let totalUsdValue = 0;
-        // const balances: AssetBalance[] = assetsData.map((asset: any) => {
-        //   const usdValue = parseFloat(asset.amount) * (usdPrices[asset.coinSymbol] || 0);
-        //   totalUsdValue += usdValue;
-        //   return {
-        //     asset: asset.coinSymbol,
-        //     free: asset.availableAmount,
-        //     locked: asset.totalAmountWithMirror,
-        //     usdValue: usdValue.toFixed(2),
-        //   };
-        // });
+        const assetSymbols = assetsData.map((asset: any) => asset.coinSymbol);
+        const prices = await fetchUsdPrices(assetSymbols);
 
-        // res.status(200).json({ balances, totalUsdValue: totalUsdValue.toFixed(2) });
+        const assetsWithUsdValue = assetsData.map((asset: any) => {
+          const usdValue = parseFloat(asset.availableAmount) * (prices[asset.coinSymbol] || 0);
+          return { ...asset, usdValue };
+        });
+        res.status(200).json({ balances: assetsWithUsdValue });
       } else {
         console.error('API Error:', response.data);
         res.status(500).json({
@@ -130,6 +122,7 @@ const mapAssetToCoinGeckoId = (asset: string): string => {
     BTC: 'bitcoin',
     USDT: 'tether',
     BNB: 'binancecoin',
+    ETH: 'ethereum',
   };
   return mapping[asset] || asset.toLowerCase();
 };
