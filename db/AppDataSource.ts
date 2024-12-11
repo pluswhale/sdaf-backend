16508d0e05b2866740c9fbd215f6ff2ae1e044eb
Sergey Lobodin <lobodin01@mail.ru>
2024-12-11 10:42:06 +0300

    fixed AppDataSource

--- db/AppDataSource.ts
+++ db/AppDataSource.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
-import { Margin, Wallet } from './entities';
+import { Margin, PendingWithdrawal, Wallet } from './entities';
 import { User } from './entities';
-import { PendingWithdrawal } from './entities/PendingWithdrawal';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const InitDataSource = () => {
   const dbHost = process.env.DB_HOST;
