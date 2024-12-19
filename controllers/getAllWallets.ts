6c7d557883994d7c558c9a94218d7463567dd8f3
Sergey Lobodin <lobodin01@mail.ru>
2024-12-19 19:10:52 +0300

    modified getAllWallets.ts with test wallets

--- controllers/getAllWallets.ts
+++ controllers/getAllWallets.ts
@@ -6,6 +6,49 @@ import { Request, Response } from 'express';
 
 const walletRepository = AppDataSource.getRepository(Wallet);
 
+type WalletProcessor = (wallet: Wallet, usdtPrice: number) => Promise<any>;
+
+const processBTC: WalletProcessor = async (wallet, usdtPrice) => {
+  const price = await checkBalanceBTCToUSDT(wallet.address);
+  return { ...wallet, price };
+};
+
+const processUSDT_BEP20: WalletProcessor = async (wallet, usdtPrice) => {
+  const usd = await checkBalanceUSDT(wallet.address);
+  const bnb = await checkBalanceInBNB(wallet.address);
+  const usdValue = parseFloat(usd) * usdtPrice;
+  return { ...wallet, price: { usd, bnb, usdValue: usdValue.toFixed(2) } };
+};
+
+const processBNB: WalletProcessor = async (wallet, usdtPrice) => {
+  const bnb = await checkBalanceInBNB(wallet.address);
+  const bnbPrice = await fetchBNBPrice();
+  const usdValue = bnb * bnbPrice;
+  return { ...wallet, price: { bnb, usdValue: usdValue.toFixed(2) } };
+};
+
+const processDefault: WalletProcessor = async (wallet) => {
+  return wallet;
+};
+
+export const walletProcessors: { [key: string]: WalletProcessor } = {
+  BTC: processBTC,
+  BTC_T: processBTC,
+  USDT_BEP20: processUSDT_BEP20,
+  USDT_CT: processUSDT_BEP20,
+  USDT_T: processUSDT_BEP20,
+  USDT_TRC20: processUSDT_BEP20,
+  USDT_ERC20: processUSDT_BEP20,
+  BNB: processBNB,
+  ETH: processBNB,
+};
+
+const testWallets: { [key: string]: boolean } = {
+  BTC_T: false,
+  USDT_CT: false,
+  USDT_T: false,
+};
+
 export const getAllWallets = async (req: Request, res: Response): Promise<any> => {
   try {
     const wallets = await walletRepository.find();
@@ -18,17 +61,8 @@ export const getAllWallets = async (req: Request, res: Response): Promise<any> =
 
     const walletsWithPrice = await Promise.all(
       wallets.map(async (wallet) => {
-        if (wallet.currency_type === 'BTC') {
-          const price = await checkBalanceBTCToUSDT(wallet.address);
-          return { ...wallet, price };
-        } else if (wallet.currency_type === 'USDT_BEP20') {
-          const usd = await checkBalanceUSDT(wallet.address);
-          const bnb = await checkBalanceInBNB(wallet.address);
-          const usdValue = parseFloat(usd) * usdtPrice;
-          return { ...wallet, price: { usd, bnb, usdValue: usdValue.toFixed(2) } };
-        } else {
-          return wallet;
-        }
+        const processor = walletProcessors[wallet.currency_type] || processDefault;
+        return await processor(wallet, usdtPrice);
       }),
     );
 
