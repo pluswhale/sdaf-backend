d25d1859b8225162048f6c1a46a7bef8189b087d
Ryu <rastsislauyushkevich@gmail.com>
2025-01-21 14:59:27 +0300

    fix: updated api keys from env

--- services/binanceTrade.ts
+++ services/binanceTrade.ts
@@ -1,27 +1,25 @@
 const Binance = require('binance-api-node').default;
 
-
 const client = Binance({
-  apiKey: 'YOUR_BINANCE_API_KEY',       
-  apiSecret: 'YOUR_BINANCE_API_SECRET', 
+  apiKey: process.env.BINANCE_API_KEY,
+  apiSecret: process.env.BINANCE_API_SECRET_KEY,
 });
 
-
-export async function placeLimitBuyOrder(priceCurrency: number, quantityAmount: number, symbolCurrency: string) { // symbol ex.BNBUSDT
+export async function placeLimitBuyOrder(priceCurrency: number, quantityAmount: number, symbolCurrency: string) {
+  // symbol ex.BNBUSDT
   try {
- 
-    const symbol =  symbolCurrency;  // Trading pair BNB/USDT
-    const quantity = quantityAmount;        // Amount of BNB you want to buy
-    const price = priceCurrency;         // The limit price you're willing to pay for BNB (in USDT)
-    
+    const symbol = symbolCurrency; // Trading pair BNB/USDT
+    const quantity = quantityAmount; // Amount of BNB you want to buy
+    const price = priceCurrency; // The limit price you're willing to pay for BNB (in USDT)
+
     // Place a limit buy order
     const order = await client.order({
       symbol: symbol,
-      side: 'BUY',               // 'BUY' side for the limit order
-      type: 'LIMIT',             // Limit order type
-      price: price.toFixed(2),   // Limit price to 2 decimal places
-      quantity: quantity,       // Quantity of BNB to buy
-      timeInForce: 'GTC',        // 'Good Till Canceled' - the order will remain open until filled or canceled
+      side: 'BUY', // 'BUY' side for the limit order
+      type: 'LIMIT', // Limit order type
+      price: price.toFixed(2), // Limit price to 2 decimal places
+      quantity: quantity, // Quantity of BNB to buy
+      timeInForce: 'GTC', // 'Good Till Canceled' - the order will remain open until filled or canceled
     });
 
     console.log('Limit buy order placed:', order);
@@ -32,15 +30,16 @@ export async function placeLimitBuyOrder(priceCurrency: number, quantityAmount: 
   }
 }
 
-
-export async function checkOrderStatus(orderId: number, symbol: string) { //symbol ex. BNBUSDT
-    try {
-      const orderStatus = await client.getOrder({
-        symbol: symbol,    // Trading pair
-        orderId: orderId,     // The ID of the order you want to check
-      });
-      return orderStatus;
-    } catch (error) {
-      console.error('Error checking order status:', error);
-    }
+export async function checkOrderStatus(orderId: number, symbol: string) {
+  //symbol ex. BNBUSDT
+  try {
+    const orderStatus = await client.getOrder({
+      symbol: symbol, // Trading pair
+      orderId: orderId, // The ID of the order you want to check
+    });
+    return orderStatus;
+  } catch (error) {
+    console.error('Error checking order status:', error);
+  }
 }
+
