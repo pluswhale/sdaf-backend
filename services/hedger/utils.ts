3e7aedc1047d6dd304528833533620f32b526a44
Mona Lisa <johndoe@example.com>
2025-01-27 15:14:00 +0300

    changed btc checker

--- services/hedger/utils.ts
+++ services/hedger/utils.ts
@@ -1,5 +1,5 @@
 export const RECEIVER_WALLETS = {
   "usdt_bnb": {walletAddress: '0x56Dd25A806D2B3096eD2d9dA220B3229313E8771', symbol: "BNB-USDT", direction: "BUY"},
   "usdt_btc": {walletAddress: '0x66FBb823E5F53D5E8161CCcd3C5BC4d158b82F27', symbol: "BTC-USDT", direction: "BUY"},
-  "btc_usdt": {walletAddress: '38WpJVhaeJPaoL4oLvtoo7wJFT8A3hQPSW', symbol: "BTC-USDT", direction: "SELL"},
+  "btc_usdt": {walletAddress: '3BubMsdYqDLuND6fqfrMRM26JsSR4odMcG', symbol: "BTC-USDT", direction: "SELL"},
 }
