e8f2b5a3960d85aac0f8dbdbe738c2ce87d320d6
Mona Liza <dultsevegorpw@gmail.com>
2025-01-31 15:01:27 +0300

    fixed sort to finalise log

--- services/hedger/utils.ts
+++ services/hedger/utils.ts
@@ -6,8 +6,8 @@ export const RECEIVER_WALLETS = {
 };
 
 export const FINALISE_WALLETS = {
-  usdt_bnb: { walletAddress: '0x56dd25a806d2b3096ed2d9da220b3229313e8771', symbol: 'BNB-USDT', direction: 'BUY' },
+  usdt_bnb: { walletAddress: '0x9671f6e0f9932145464713ae877ce8f4795f6158', symbol: 'BNB-USDT', direction: 'BUY' },
   usdt_btc: { walletAddress: '0x168e3f5919fC3858D7911b3e302B826f0dE6B10b', symbol: 'BTC-USDT', direction: 'BUY' },
-  bnb_usdt: { walletAddress: '0x56dd25a806d2b3096ed2d9da220b3229313e8771', symbol: 'BNB-USDT', direction: 'SELL' },
+  bnb_usdt: { walletAddress: '0x9671f6e0f9932145464713ae877ce8f4795f6158', symbol: 'BNB-USDT', direction: 'SELL' },
   btc_usdt: { walletAddress: '3BkiS2yLAJPBeUqoMkfADDyBEVue4QgPiV', symbol: 'BTC-USDT', direction: 'SELL' },
 };
