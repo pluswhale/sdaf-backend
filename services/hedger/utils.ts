bad541de1ae7a7ad3733b9c684e3b8938e663e00
Mona Liza <dultsevegorpw@gmail.com>
2025-02-03 15:45:53 +0300

    changed receiver swaps

--- services/hedger/utils.ts
+++ services/hedger/utils.ts
@@ -1,7 +1,7 @@
 export const RECEIVER_WALLETS = {
-  usdt_bnb: { walletAddress: '0x56Dd25A806D2B3096eD2d9dA220B3229313E8771', symbol: 'BNB-USDT', direction: 'BUY' },
+  usdt_bnb: { walletAddress: '0x66FBb823E5F53D5E8161CCcd3C5BC4d158b82F27', symbol: 'BNB-USDT', direction: 'BUY' },
   usdt_btc: { walletAddress: '0x66FBb823E5F53D5E8161CCcd3C5BC4d158b82F27', symbol: 'BTC-USDT', direction: 'BUY' },
-  bnb_usdt: { walletAddress: '0x56Dd25A806D2B3096eD2d9dA220B3229313E8771', symbol: 'BNB-USDT', direction: 'SELL' },
+  bnb_usdt: { walletAddress: '0x66FBb823E5F53D5E8161CCcd3C5BC4d158b82F27', symbol: 'BNB-USDT', direction: 'SELL' },
   btc_usdt: { walletAddress: '3BubMsdYqDLuND6fqfrMRM26JsSR4odMcG', symbol: 'BTC-USDT', direction: 'SELL' },
 };
 
@@ -11,3 +11,4 @@ export const FINALISE_WALLETS = {
   bnb_usdt: { walletAddress: '0x168e3f5919fC3858D7911b3e302B826f0dE6B10b', symbol: 'BNB-USDT', direction: 'SELL' },
   btc_usdt: { walletAddress: '3BkiS2yLAJPBeUqoMkfADDyBEVue4QgPiV', symbol: 'BTC-USDT', direction: 'SELL' },
 };
+
