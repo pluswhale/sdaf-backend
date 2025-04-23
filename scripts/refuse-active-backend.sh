b1e6b580f2e66f14f4a1bfe11660a54316c9b706
achisty <andreichisty@coinweb.io>
2025-04-23 17:10:30 +0300

    fix: deploy

--- scripts/refuse-active-backend.sh
+++ scripts/refuse-active-backend.sh
@@ -6,8 +6,7 @@ readonly ALLOW_DEPLOY_TO_ACTIVE_BACKEND
 
 main() {
     if [[ -n $ALLOW_DEPLOY_TO_ACTIVE_BACKEND ]]; then
-        echo 'WARNING: variable ALLOW_DEPLOY_TO_ACTIVE_BACKEND is set. '`
-            `'Check of the active backend will be skipped
+        echo "WARNING: variable ALLOW_DEPLOY_TO_ACTIVE_BACKEND is set. Check of the active backend will be skipped"
         exit 0
     fi
 
@@ -15,29 +14,26 @@ main() {
     local -r prod_color=$(get_prod_color)
 
     if [[ -z $prod_color ]]; then
-        echo -e >&2 \
-            'ERROR: unable to get the active backend color!\n'`
-           `'You can set the ALLOW_DEPLOY_TO_ACTIVE_BACKEND variable to any value to skip the check'
+        echo -e >&2 "ERROR: unable to get the active backend color!\nYou can set the ALLOW_DEPLOY_TO_ACTIVE_BACKEND variable to any value to skip the check"
         exit 1
     fi
+
     printf 'Active backend: %s\n' "$prod_color"
 
     if [[ $color == "$prod_color" ]]; then
-        echo -e >&2 \
-            'ERROR: deploying to the active backend is not allowed!\n'`
-           `'To skip this check set the ALLOW_DEPLOY_TO_ACTIVE_BACKEND variable to any value'
+        echo -e >&2 "ERROR: deploying to the active backend is not allowed!\nTo skip this check set the ALLOW_DEPLOY_TO_ACTIVE_BACKEND variable to any value"
         exit 1
     fi
+
     exit 0
 }
 
 get_prod_color() {
     local -r external_name=$(kubectl get service \
-    -n sdaf-cwap-production-services \
-    -o jsonpath='{.items[0].spec.externalName}' \
-    | grep -Eo 'production-sdaf-backend-service[^"]*'
+        -n sdaf-cwap-production-services \
+        -o jsonpath='{.items[?(@.metadata.name == "production-sdaf-backend-service")].spec.externalName}'
     )
-    echo "${external_name%-sdaf-backend-service.*}"
+    echo "${external_name%-app-green.sdafcwap.*}"
 }
 
 main "$@"
