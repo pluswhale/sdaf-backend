6e9631d4a2158486e6aae45828795e9063c6a9d9
Mona Liza <dultsevegorpw@gmail.com>
2025-05-28 15:02:37 +0300

    added cors

--- server.ts
+++ server.ts
@@ -25,6 +25,7 @@ app.use(
       'http://localhost:3000',
       'https://sdafcwap.com',
       'https://blue.sdafcwap.com',
+      'https://staging.sdafcwap.com',
       'http://localhost:5000',
       'https://cwap3.coinhq.store',
       'https://app.cwap.io/botHedging',
