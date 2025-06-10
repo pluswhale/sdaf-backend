bc354131cba41919ac8080e38d55b260b1143811
Ryu <rastsislauyushkevich@gmail.com>
2025-06-10 11:09:32 +0300

    fixed permission checker

--- middlewares/checkPermission.ts
+++ middlewares/checkPermission.ts
@@ -2,28 +2,24 @@ import { NextFunction, Request, Response } from 'express';
 import { Permission } from '../db/entities/Permission';
 import jwt from 'jsonwebtoken';
 import { AppDataSource } from '../db/AppDataSource';
+import { User } from '../db/entities';
 
-const permissionsRepo = AppDataSource.getRepository(Permission);
+const usersRepo = AppDataSource.getRepository(User);
 
 export const checkPermission = (permission: string) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.replace('Bearer ', '');
-    let requiredPermission: Permission | null;
-    try {
-      requiredPermission = await permissionsRepo.findOne({ where: { name: permission } });
-    } catch (error) {
-      console.error('Error retreiving required permission from the database', error);
-    }
 
     if (token) {
-      let userPermissions: number[];
       //@ts-ignore
-      userPermissions = jwt.verify(token, process.env.SECRET_JWT_KEY).user.permissions;
+      const userId = jwt.verify(token, process.env.SECRET_JWT_KEY).user.id;
+      const user = await usersRepo.findOne({ where: { id: userId } });
+      const userPermissions: Permission[] = user?.permissions!;
       if (!userPermissions) {
         res.status(403).send({ error: 'Access denied' });
         return;
       }
-      if (!userPermissions.some((userPermission: number) => userPermission === requiredPermission?.id)) {
+      if (!userPermissions.some((userPermission: Permission) => userPermission.name === permission)) {
         res.status(403).send({ error: `Current user is lacking the ${permission} permission` });
         return;
       }
@@ -34,3 +30,4 @@ export const checkPermission = (permission: string) => {
     return next();
   };
 };
+
