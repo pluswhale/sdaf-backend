94fe6356c22ec77ffc1e54594d1a98be2ab8ce1c
AbzMijon <egorwebdev8@gmail.com>
2025-06-10 14:13:15 +0300

    update permissions seeder

--- db/seeders/PermissionSeeder.ts
+++ db/seeders/PermissionSeeder.ts
@@ -7,24 +7,24 @@ export const permissionSeeder = async (dataSource: DataSource) => {
     const userRepository = dataSource.getRepository(User);
 
     const permissions = [
-        { name: 'Send funds manually from Dashboard', category: 'funds' },
-        { name: 'Send funds manually from Archive', category: 'funds' },
-        { name: 'Send funds manually from Test Dashboard', category: 'funds' },
+        { name: 'Send funds manually from Dashboard', category: 'dashboard' },
+        { name: 'Send funds manually from Archive', category: 'dashboard' },
+        { name: 'Send funds manually from Test Dashboard', category: 'dashboard' },
         { name: 'Run/stop Rebalancer on Dashboard', category: 'rebalancer' },
         { name: 'Run/stop Rebalancer in Archive', category: 'rebalancer' },
         { name: 'Run/stop Rebalancer on Test Dashboard', category: 'rebalancer' },
+        { name: 'Configure AutoSend at Dashboard', category: 'autosender' },
+        { name: 'Configure AutoSend at Test', category: 'autosender' },
+        { name: 'Configure AutoSend at Archive', category: 'autosender' },
         { name: 'Run/stop Hedger on Production', category: 'hedger' },
         { name: 'Run/stop Test Hedger', category: 'hedger' },
-        { name: 'Configure AutoSend at Dashboard', category: 'autosend' },
-        { name: 'Configure AutoSend at Test', category: 'autosend' },
-        { name: 'Configure AutoSend at Archive', category: 'autosend' },
-        { name: 'Download csv-file', category: 'another' },
-        { name: 'Setup margin', category: 'another' },
-        { name: 'Archive/unarchive wallets', category: 'another' },
-        { name: 'Edit coins to show at Pairs tab', category: 'another' },
-        { name: 'Setup bot order parameters', category: 'another' },
-        { name: 'Modify wallets on Test Dasboard', category: 'another' },
-        { name: 'View Customer Dashboard', category: 'another' },
+        { name: 'Download csv-file', category: 'other' },
+        { name: 'Setup margin', category: 'other' },
+        { name: 'Archive/unarchive wallets', category: 'other' },
+        { name: 'Edit coins to show at Pairs tab', category: 'other' },
+        { name: 'Setup bot order parameters', category: 'other' },
+        { name: 'Modify wallets on Test Dasboard', category: 'other' },
+        { name: 'View Customer Dashboard', category: 'other' },
     ];
 
     for (const permissionData of permissions) {
