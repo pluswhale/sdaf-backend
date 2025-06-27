a1fea3071b77b9e1717e221549f4907a79f1e2c4
AbzMijon <egorwebdev8@gmail.com>
2025-06-27 13:03:27 +0300

    add body size ingress

--- server.ts
+++ server.ts
@@ -19,8 +19,9 @@ if (process.env.GITLAB_ENVIRONMENT_NAME && process.env.GITLAB_ENVIRONMENT_NAME.i
 }
 
 const app = express();
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-app.use(express.json());
 app.use(
   cors({
     origin: [
