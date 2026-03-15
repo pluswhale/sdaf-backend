8491d92c0980e01d4cb43872af66c5f5af213d3f
Mona Liza <dultsevegorpw@gmail.com>
2026-03-15 12:55:58 +0300

    dkdkd

--- db/AppDataSource.ts
+++ db/AppDataSource.ts
@@ -20,6 +20,8 @@ import { HedgerConfigOptions } from './entities/HedgerConfigOptions';
 
 dotenv.config();
 
+console.log('process.env.DB_URL: ' + process.env.DB_URL);
+
 const InitDataSource = () => {
   const dbHost = process.env.DB_HOST;
 
@@ -44,11 +46,18 @@ const InitDataSource = () => {
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '12345',
-  database: process.env.DB_NAME || 'test',
+  // host: process.env.DB_HOST || 'localhost',
+  // port: parseInt(process.env.DB_PORT || '5432'),
+  // username: process.env.DB_USER || 'postgres',
+  // password: process.env.DB_PASSWORD || '12345',
+  // database: process.env.DB_NAME || 'test',
+  url: process.env.DB_URL || '',
+  ssl: { rejectUnauthorized: false },
+  extra: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
   synchronize: true,
   logging: false,
   entities: [
